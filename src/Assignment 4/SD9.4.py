9e04d7170e2cd7d4e354f711e7246b08f3b786f4
Michael Dhamers <doomerz@comcast.net>
2026-03-08 18:29:19 -0400

    small update to SD9.4

--- src/Assignment 4/SD9.4.py
+++ src/Assignment 4/SD9.4.py
@@ -4,8 +4,10 @@
 # plot f along with partial sum of the fourier series synthesis, the first 50 nonzero components over -T<=t<=T
 # a_0 = 2/T int(y(t)) = should be 0 over the period #2(a*t**2/2 + c)/T, c = 0 therefore: a_0 = a*t**2/T
 # a_n = 0 since the function is odd over the period
-# b_n = (2/T)*(a*(1/w**2*sin(wt)-t/w*cos(wt)))
+# b_n = (2/T)*(a*(1/w**2*sin(wt)-t/w*cos(wt))) < this has to be evaluated from -T/2 to T/2!!!
+#^ how did I screw this up??
 import numpy as np
+import sympy as sp
 import matplotlib.pyplot as plt
 
 #consts
@@ -23,17 +25,23 @@ count = 0
 y = np.empty(((len(time),partials)))
 res = np.empty(len(time))
 while True:
+    b_n = (2/T)*(a*(1/w**2*np.sin(w*T/2)-T/2/w*np.cos(w*T/2)))
+    #old value: (2/T)*(a*(1/w**2*np.sin(w*t)-t/w*np.cos(w*t)))*np.sin(w*t)
     if count >= partials:
         break
+    if b_n == 0:
+        print(f"skipping w={w} since b_n is zero")
+        w += 1
+        continue
     for i, t in enumerate(time):
-        y[i,count] = (2/T)*(a*(1/w**2*np.sin(w*t)-t/w*np.cos(w*t)))*np.sin(w*t)
+        y[i,count] = b_n*np.sin(w*t)
     count += 1
-    w += 2
+    w += 1
 for i, x in enumerate(y):
-    res[i] = np.sum(x[1])
+    res[i] = np.sum(x)
 ax.plot(time, res, label="sum of first 50 nonzero components")
 for i in range(partials):
-    ax.plot(time, y[:,i], label=f"partial {i*2+1}")
+    ax.plot(time, y[:,i], label=f"partial {i+1}")
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("Output")
 ax.legend()
